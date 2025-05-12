ac0291ed35ad23fe16b7954dd326f024a9e39c98
Reshoan <reshoanyazdi@gmail.com>
2025-05-13 00:55:12 +0600

    trying things

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -1,30 +1,71 @@
 <?php
-require_once(__DIR__ . '/../config/db.php');
-require_once(__DIR__ . '/../models/UserModel.php');
+// RegisterController.php
+require_once '../models/UserModel.php';
 
+class RegisterController
+{
+    public function registerUser()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userData = [
+                'name' => $_POST['name'],
+                'password' => $_POST['password'],
+                'address' => $_POST['address'],
+                'phone' => $_POST['phone'],
+                'email' => $_POST['email'],
+                'type' => $_POST['type']
+            ];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $user = new UserModel();
+            $spData = null;
+            $spTypeData = null;
 
-    $data = [
-        'id' => $_POST['us_id'],
-        'name' => $_POST['us_name'],
-        'password' => $_POST['us_password'],
-        'address' => $_POST['us_address'],
-        'phone' => $_POST['us_phone_no'],
-        'email' => $_POST['us_email'],
-        'type' => $_POST['us_type'],
-        'experience' => isset($_POST['sp_experience']) ? $_POST['sp_experience'] : null,
-        'gender' => isset($_POST['sp_gender']) ? $_POST['sp_gender'] : null,
-        'dob' => isset($_POST['sp_dob']) ? $_POST['sp_dob'] : null,
-        'nid' => isset($_POST['sp_nid_no']) ? $_POST['sp_nid_no'] : null,
-        'salary' => isset($_POST['sp_expected_salary']) ? $_POST['sp_expected_salary'] : null,
-        'education' => isset($_POST['sp_education']) ? $_POST['sp_education'] : null,
-        'certification' => isset($_POST['sp_certification']) ? $_POST['sp_certification'] : null,
-        'sp_type' => isset($_POST['sp_type']) ? $_POST['sp_type'] : null
-    ];
+            if ($userData['type'] === 'service_provider') {
+                $spData = [
+                    'experience' => $_POST['experience'],
+                    'gender' => $_POST['gender'],
+                    'dob' => $_POST['dob'],
+                    'nid' => $_POST['nid'],
+                    'expected_salary' => $_POST['expected_salary'],
+                    'education' => $_POST['education'],
+                    'certification' => $_POST['certification'],
+                    'sp_type' => $_POST['sp_type']
+                ];
 
-    $user->registerUser($data);
-    header("Location: ../views/register.php?success=1");
-    exit;
+                switch ($spData['sp_type']) {
+                    case 'cook':
+                        $spTypeData = [
+                            'cuisine_expertise' => $_POST['cuisine_expertise'],
+                            'max_people_served' => $_POST['max_people_served']
+                        ];
+                        break;
+                    case 'chauffer':
+                        $spTypeData = [
+                            'vehicle_types' => $_POST['vehicle_types'],
+                            'licence_doc' => $_POST['licence_doc'],
+                            'licence_valid_until' => $_POST['licence_valid_until']
+                        ];
+                        break;
+                    case 'cleaners':
+                        $spTypeData = [
+                            'employment_type' => $_POST['employment_type']
+                        ];
+                        break;
+                    // Add other service provider types similarly
+                }
+            }
+
+            $userModel = new UserModel();
+            $result = $userModel->register($userData, $spData, $spTypeData);
+
+            if ($result) {
+                echo "Registration successful!";
+            } else {
+                echo "Registration failed.";
+            }
+        }
+    }
 }
+
+// Usage (if this file is hit directly)
+$controller = new RegisterController();
+$controller->registerUser();
--- models/UserModel.php
+++ models/UserModel.php
@@ -1,65 +1,150 @@
 <?php
-require_once(__DIR__ . '/../config/db.php');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
-class UserModel {
-    private $conn;
+class UserModel
+{
+    private $pdo;
+    private $oci;
 
-    public function __construct() {
-        // Initialize the database connection
-        $db = new Database();
-        $this->conn = $db->getConnection();
+    public function __construct($pdo, $oci)
+    {
+        $this->pdo = $pdo;
+        $this->oci = $oci;
     }
 
-    public function registerUser($data) {
+    private function getNextUserId()
+    {
+        $query = "SELECT user_seq.NEXTVAL AS next_id FROM dual";
+        $stid = oci_parse($this->oci, $query);
+        oci_execute($stid);
+        $row = oci_fetch_assoc($stid);
+        return $row['NEXT_ID'];
+    }
+
+    public function register($data)
+    {
         try {
-            // No password hashing, store plain text password
-            $plainPassword = $data['password'];
+            $this->pdo->beginTransaction();
 
-            // Insert the user data into the Users table
-            $query = "INSERT INTO Users (us_id, us_name, us_password, us_address, us_phone_no, us_email, us_type)
-                      VALUES (:id, :name, :password, :address, :phone, :email, :type)";
-            $stmt = $this->conn->prepare($query);
+            // Get next user ID
+            $userId = $this->getNextUserId();
 
-            $stmt->bindParam(':id', $data['id']);
-            $stmt->bindParam(':name', $data['name']);
-            $stmt->bindParam(':password', $plainPassword); // Storing plain password
-            $stmt->bindParam(':address', $data['address']);
-            $stmt->bindParam(':phone', $data['phone']);
-            $stmt->bindParam(':email', $data['email']);
-            $stmt->bindParam(':type', $data['type']);
+            // 1. Insert into Users
+            $sqlUser = "INSERT INTO Users (us_id, us_name, us_password, us_address, us_phone_no, us_email, us_type)
+                        VALUES (:us_id, :us_name, :us_password, :us_address, :us_phone_no, :us_email, :us_type)";
+            $stmtUser = $this->pdo->prepare($sqlUser);
+            $stmtUser->execute([
+                ':us_id' => $userId,
+                ':us_name' => $data['us_name'],
+                ':us_password' => password_hash($data['us_password'], PASSWORD_DEFAULT),
+                ':us_address' => $data['us_address'],
+                ':us_phone_no' => $data['us_phone_no'],
+                ':us_email' => $data['us_email'],
+                ':us_type' => $data['us_type']
+            ]);
 
-            $stmt->execute();
+            if ($data['us_type'] === 'service_provider') {
+                // 2. Insert into Service_Provider
+                $sqlSP = "INSERT INTO Service_Provider (sp_id, sp_experience, sp_gender, sp_dob, sp_nid_no, sp_expected_salary, sp_education, sp_certification, sp_type)
+                          VALUES (:sp_id, :sp_experience, :sp_gender, TO_DATE(:sp_dob, 'YYYY-MM-DD'), :sp_nid_no, :sp_expected_salary, :sp_education, :sp_certification, :sp_type)";
+                $stmtSP = $this->pdo->prepare($sqlSP);
+                $stmtSP->execute([
+                    ':sp_id' => $userId,
+                    ':sp_experience' => $data['sp_experience'],
+                    ':sp_gender' => $data['sp_gender'],
+                    ':sp_dob' => $data['sp_dob'],
+                    ':sp_nid_no' => $data['sp_nid_no'],
+                    ':sp_expected_salary' => $data['sp_expected_salary'],
+                    ':sp_education' => $data['sp_education'],
+                    ':sp_certification' => $data['sp_certification'],
+                    ':sp_type' => $data['sp_type']
+                ]);
 
-            if ($data['type'] === 'service_provider') {
-                $spQuery = "INSERT INTO Service_Provider (
-                    sp_id, sp_experience, sp_gender, sp_dob, sp_nid_no,
-                    sp_expected_salary, sp_education, sp_certification, sp_type
-                ) VALUES (
-                    :id, :experience, :gender, TO_DATE(:dob, 'YYYY-MM-DD'), :nid, :salary,
-                    :education, :certification, :sp_type)";
+                // 3. Insert into specific service table
+                $spType = strtolower(str_replace(' ', '', $data['sp_type']));
+                switch ($spType) {
+                    case 'cook':
+                        $sql = "INSERT INTO Cook_Details (sp_id, ck_cuisine_expertise, ck_max_people_served)
+                                VALUES (:sp_id, :ck_cuisine_expertise, :ck_max_people_served)";
+                        $stmt = $this->pdo->prepare($sql);
+                        $stmt->execute([
+                            ':sp_id' => $userId,
+                            ':ck_cuisine_expertise' => $data['ck_cuisine_expertise'],
+                            ':ck_max_people_served' => $data['ck_max_people_served']
+                        ]);
+                        break;
 
-                $spStmt = $this->conn->prepare($spQuery);
-                $spStmt->bindParam(':id', $data['id']);
-                $spStmt->bindParam(':experience', $data['experience']);
-                $spStmt->bindParam(':gender', $data['gender']);
-                $spStmt->bindParam(':dob', $data['dob']);
-                $spStmt->bindParam(':nid', $data['nid']);
-                $spStmt->bindParam(':salary', $data['salary']);
-                $spStmt->bindParam(':education', $data['education']);
-                $spStmt->bindParam(':certification', $data['certification']);
-                $spStmt->bindParam(':sp_type', $data['sp_type']);
+                    case 'chauffer':
+                        $sql = "INSERT INTO Chauffer_Details (sp_id, ch_vehicle_types, ch_licence_doc, ch_licence_valid_until)
+                                VALUES (:sp_id, :ch_vehicle_types, :ch_licence_doc, TO_DATE(:ch_licence_valid_until, 'YYYY-MM-DD'))";
+                        $stmt = $this->pdo->prepare($sql);
+                        $stmt->execute([
+                            ':sp_id' => $userId,
+                            ':ch_vehicle_types' => $data['ch_vehicle_types'],
+                            ':ch_licence_doc' => $data['ch_licence_doc'],
+                            ':ch_licence_valid_until' => $data['ch_licence_valid_until']
+                        ]);
+                        break;
 
-                $spStmt->execute();
+                    case 'cleaners':
+                        $sql = "INSERT INTO Cleaner_Details (sp_id, cl_employment_type)
+                                VALUES (:sp_id, :cl_employment_type)";
+                        $stmt = $this->pdo->prepare($sql);
+                        $stmt->execute([
+                            ':sp_id' => $userId,
+                            ':cl_employment_type' => $data['cl_employment_type']
+                        ]);
+                        break;
+
+                    case 'securityguard':
+                        $sql = "INSERT INTO SecurityGuard_Details (sp_id, sg_weapons_training)
+                                VALUES (:sp_id, :sg_weapons_training)";
+                        $stmt = $this->pdo->prepare($sql);
+                        $stmt->execute([
+                            ':sp_id' => $userId,
+                            ':sg_weapons_training' => $data['sg_weapons_training']
+                        ]);
+                        break;
+
+                    case 'relocators':
+                        $sql = "INSERT INTO Relocator_Details (sp_id, rl_vehicle_type)
+                                VALUES (:sp_id, :rl_vehicle_type)";
+                        $stmt = $this->pdo->prepare($sql);
+                        $stmt->execute([
+                            ':sp_id' => $userId,
+                            ':rl_vehicle_type' => $data['rl_vehicle_type']
+                        ]);
+                        break;
+
+                    case 'babysitter':
+                        $sql = "INSERT INTO Babysitter_Details (sp_id, bs_languages)
+                                VALUES (:sp_id, :bs_languages)";
+                        $stmt = $this->pdo->prepare($sql);
+                        $stmt->execute([
+                            ':sp_id' => $userId,
+                            ':bs_languages' => $data['bs_languages']
+                        ]);
+                        break;
+
+                    case 'masseuse':
+                        $sql = "INSERT INTO Masseuse_Details (sp_id, ms_speciality)
+                                VALUES (:sp_id, :ms_speciality)";
+                        $stmt = $this->pdo->prepare($sql);
+                        $stmt->execute([
+                            ':sp_id' => $userId,
+                            ':ms_speciality' => $data['ms_speciality']
+                        ]);
+                        break;
+
+                    // plumber, electrician, gardener can be added similarly when schema is available
+                }
             }
 
-            echo "User registration successful!";
-        } catch (PDOException $e) {
-            error_log($e->getMessage());
-            die("PDO Error: " . $e->getMessage());
+            $this->pdo->commit();
+            return true;
+        } catch (Exception $e) {
+            $this->pdo->rollBack();
+            error_log("Registration failed: " . $e->getMessage());
+            return false;
         }
     }
-    
-
 }
